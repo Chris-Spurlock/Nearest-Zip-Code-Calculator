81c32b094365dc2b84a98f9dfb924cbb382af48d
Chris Spurlock <christopher.spurlock@eltoro.com>
2020-12-30 13:30:53 -0500

    Resolved NaN distances by converting lat/lng of target zip to float

--- nearest_zips.py
+++ nearest_zips.py
@@ -3,17 +3,24 @@ import pandas as pd
 
 zip_df = pd.read_csv('zip_w_lat-long.csv')
 
+# Todo: export results to csv and db table
 # Todo: try block to ensure zip is valid
 def find_zips(zipcode):
     """
     Take a ZIP code as a parameter and find all other ZIP codes within a 200-mile radius.
     """
+    MAX_DISTANCE = 200
+
     target_zip = int(zipcode)
-    target_lat = zip_df.loc[zip_df['zip'] == target_zip]['lat']
-    target_long = zip_df.loc[zip_df['zip'] == target_zip]['long']
+    target_lat = float(zip_df.loc[zip_df['zip'] == target_zip]['lat'])
+    target_long = float(zip_df.loc[zip_df['zip'] == target_zip]['long'])
 
     zip_df['distance'] = haversine_distance(target_lat, target_long, zip_df['lat'], zip_df['long'])
-    nearest_zips = zip_df[zip_df['distance'] <= 200]
+    nearest_zips = zip_df[zip_df['distance'] <= MAX_DISTANCE]
+    nearest_zips['target_zipcode'] = str(target_zip)
+    nearest_zips = nearest_zips.drop(['lat', 'long'], axis=1)
+    nearest_zips.columns = ['nearby_zipcode', 'distance', 'target_zipcode']
+    nearest_zips = nearest_zips[['target_zipcode', 'nearby_zipcode', 'distance']] # could probably do this bit more efficiently
 
     return nearest_zips
 
@@ -26,10 +33,11 @@ def haversine_distance(lat1, long1, lat2, long2):
     """
     MILES = 3959 # Radius of Earth in mi
 
-    # convert decimal degrees to radians 
+    # convert decimal degrees to radians
     lat1, long1, lat2, long2 = map(np.deg2rad, [lat1, long1, lat2, long2])
 
-    # haversine formula 
+    # haversine formula
+    # shitty variable names (fix)
     dlong = long2 - long1
     dlat = lat2 - lat1
     a = np.sin(dlat/2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlong/2)**2
